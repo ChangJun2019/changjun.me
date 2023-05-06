7ab254750bf3c98eb9739e825f52cf2bdf483881
changjun <52chinaweb@gmail.com>
2023-05-06 23:09:30 +0800

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,7 @@ export default defineNuxtConfig({
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/img/blog-apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/blog-favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/blog-favicon-16x16.png' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.icon' },
         { rel: 'stylesheet', href: 'https://cdn.staticfile.org/lxgw-wenkai-screen-webfont/1.7.0/style.min.css' },
       ],
     },
