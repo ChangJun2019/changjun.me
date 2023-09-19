c2ab9e9d46fdd9da21fc2cdbd0d594d5214be190
changjun <52chinaweb@gmail.com>
2023-09-19 10:56:11 +0800

    feat: add book

--- components/Book/BookPostItem.vue
+++ components/Book/BookPostItem.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
-
+const props = defineProps<{
+  title: string
+  cover: string
+}>()
 </script>
 
 <template>
   <article class="group">
     <img
       alt="Lava"
-      src="https://images.unsplash.com/photo-1631451095765-2c91616fc9e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+      :src="props.cover"
       class="h-56 w-full rounded-xl object-cover shadow-xl transition hover:scale-105"
-    />
+    >
 
     <div class="p-4">
       <a href="#">
-        <h3 class="lg:text-lg font-medium text-center">
-          2023.08
+        <h3 class="text-center font-medium lg:text-lg">
+          {{ props.title }}
         </h3>
       </a>
-  </div>  
-</article>
+    </div>
+  </article>
 </template>
--- components/Book/BookPostList.vue
+++ components/Book/BookPostList.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+const books = await queryContent('books').only(['title', 'cover', '_id']).find()
 </script>
 
 <template>
-  <div class="grid gap-4 mt-8 sm:grid-cols-2 2xl:grid-cols-3">
-    <BookPostItem v-for="x in 1" :key="x" />
+  <div class="grid mt-8 gap-4 2xl:grid-cols-3 sm:grid-cols-2">
+    <BookPostItem v-for="book in books" :key="book._id" :title="book.title!" :cover="book.cover" />
   </div>
 </template>
