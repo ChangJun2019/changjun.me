13a053d8452a824efdde20939a3f8cdb6880463f
Chang Jun <52chinaweb@gmail.com>
2023-11-01 11:53:14 +0800

    feat: use nuxt image

--- components/HomeGallery.vue
+++ components/HomeGallery.vue
@@ -4,11 +4,11 @@
 <template>
   <div class="grid grid-cols-2 animate-zoom-in-right gap-4">
     <div class="grid gap-4">
-      <img class="aspect-auto w-full rounded-lg hover:animate-wobble" src="/images/blog-home-we.webp" alt="">
+      <NuxtImg class="aspect-auto w-full rounded-lg hover:animate-wobble" width="248" height="372" sizes="175px md:248px" src="/images/blog-home-we.webp" alt="a group photo of a couple" loading="lazy" />
     </div>
     <div class="grid gap-4">
-      <img class="aspect-auto w-full rounded-lg hover:animate-wobble" src="/images/blog-home-max.webp" alt="">
-      <img class="aspect-auto w-full rounded-lg hover:animate-wobble" src="/images/blog-home-wangzai.webp" alt="">
+      <NuxtImg class="aspect-auto w-full rounded-lg hover:animate-wobble" width="248" height="226" sizes="175px md:248px" src="/images/blog-home-max.webp" alt="a cute dog named Max " loading="lazy" />
+      <NuxtImg class="aspect-auto w-full rounded-lg hover:animate-wobble" width="248" height="226" sizes="175px md:248px" src="/images/blog-home-wangzai.webp" alt="a cute dog named WangZai " loading="lazy" />
     </div>
   </div>
 </template>
