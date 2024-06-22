361de8f7d5ecd2418584e9f83f4dc1f7534f4149
Jun Chang <52chinaweb@gmail.com>
2024-06-22 17:20:51 +0800

    ✨ feat(website): update content

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,7 +7,7 @@
       <a class="border-b-1 border-dashed transition duration-500 hover:border-primary hover:border-solid hover:text-primary" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
     </p>
     <p mt-2>
-      © 2023 Chang Jun. All Rights Reserved.
+      © 2024 Chang Jun. All Rights Reserved.
     </p>
     <div class="mt-6 flex justify-center">
       <div>
